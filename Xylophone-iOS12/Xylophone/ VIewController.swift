ad1fbbb7cc8177eff62d03cb78d36c48c41c49e9
Marty McAlpine <SnowFox@Martys-MacBook-Pro.local>
2019-07-20 11:39:48 -0700

    completed Xylophone app, cloned Quiz app

--- Xylophone-iOS12/Xylophone/ VIewController.swift
+++ Xylophone-iOS12/Xylophone/ VIewController.swift
@@ -12,8 +12,8 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate{
     
     var audioPlayer: AVAudioPlayer!
+    let soundsArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -21,23 +21,28 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-//  all seven buttons are linked to this one IBACTION, each button is tagged with a number
+//  all seven buttons are linked to this one IBAcation, each button is tagged with a number
 //  select a button, utilities pane, connections inspector, attributes inspector
 //  the sender, of type UIButton, passed in, is the button that triggered the IBAction
-        let soundUrl = Bundle.main.url(forResource: "note1", withExtension: "wav")
-//  soundUrl sets the location of the audio file to be played
+        playSound(soundFileName: soundsArray[sender.tag - 1])
+        print(sender.tag)
+        
+    }
+    
+    func playSound(soundFileName: String) {
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
+        //  soundUrl sets the location of the audio file to be played
         do {
-            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
-//  audioPlayer will use AVAudioPlayer to actually play the sound file, soundUrl
-//  with 'do try' assigning audioPlayer with an AVAudioPlayer initializer, passing in soundUrl
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+            //  audioPlayer will use AVAudioPlayer to actually play the sound file, soundUrl
+            //  with 'do try' assigning audioPlayer with an AVAudioPlayer initializer, passing in soundUrl
         }
         catch {
             print(error)
         }
-//  if the 'try' in the do block failes and throws and error, the catch block will print the error
+        //  if the 'try' in the do block failes and throws and error, the catch block will print the error
         audioPlayer.play()
     }
-    
   
 
 }
