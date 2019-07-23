1b61dcbef13ba5abdb20669f8df43b4a95b3411c
FalconRidge <falconridge@FalconRidges-Mac-Pro.local>
2019-07-23 16:01:50 -0700

    updates to viewController

--- Quizzler-iOS12/Quizzler/ViewController.swift
+++ Quizzler-iOS12/Quizzler/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
+    let allQuestion = QuestionBank()
+    
+    var pickedAnswer: Bool = false
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -18,15 +21,26 @@ class ViewController: UIViewController {
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
-//  this gets called when the UIViewController is loaded into memory whne the app starts
+//  this gets called when the UIViewController is loaded into memory when the app starts
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let firstQuestion = allQuestion.list[0]
+        questionLabel.text = firstQuestion.questionText
         
     }
 
 //  this function is called when either the true or false button is pressed
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+//  couldn't I just use else pickedAnswer = false, or do I even need to set it as false since the default option is false already?
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
     }
     
 //  this method will update all the views on screen (progress bar, score label, etc)
@@ -41,7 +55,7 @@ class ViewController: UIViewController {
     
 //  method will check if the user has got the right answer
     func checkAnswer() {
-        
+//  I need to compare the pickedAnswer against the correctAnswer method of the question instance
     }
     
 //  method will wipe the board clean, so that users can retake the quiz
